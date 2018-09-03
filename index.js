da519368b382ffc646083d3dcc9d177d2ad01faf
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-03 00:22:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
-  }
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  };
 
   return names;
-}
+};
 
 function tailsNeverFails() {
 
