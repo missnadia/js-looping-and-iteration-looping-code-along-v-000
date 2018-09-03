8f0a89e877140d8c282efdd66caa7bd06a76fe70
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-03 00:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const names = ["Ada", "Brendan", "Ali"];
+
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
     console.log(`Welcome ${names[i]}! You are employee #${i}.`)
